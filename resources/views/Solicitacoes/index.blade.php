bc135616ec6e556281b8d1a3fa16a088a83f8f2d
Joedson Silva <joeyaika@gmail.com>
2022-10-06 21:35:37 -0300

    Aprendendo mais sobre o blade

--- resources/views/Solicitacoes/index.blade.php
+++ resources/views/Solicitacoes/index.blade.php
@@ -1,4 +1,3 @@
-<div class="p-3 mb-2 bg-secondary bg-gradient text-white bg-opacity-75">
 <x-layout title="Solicitações em Andamento">
 
     @isset($mensagemSucesso)
@@ -7,28 +6,36 @@
     </div>
     @endisset
     <a href="{{route('home.create')}}" class="btn btn-primary">Solicitar</a>
-    
-    <ul class="list-group">
-         @foreach ($solicitacoes as $solicitacao)
-         <li class="list-group-item list-group-item-primary">Solicitações</li>
-         <li class="list-group-item  d-flex justify-content-between align-items-center">{{$solicitacao->nome}}
+    <div class="container">
+<div class="card table-responsive-sm " style="width: 75rem;">
+    <div class="card-header">
+    <table class="table table-borderless">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Prontuario</th>
+        </tr>
+          
+        </thead>
+    </div>
+        <tbody>
+          <tr>           
+            @foreach ($solicitacoes as $solicitacao)
             
+            <td>{{$solicitacao->nome}}</td>
+            <td>{{$solicitacao->prontuario}}</td>
+            <th>
             <form action="{{route('home.destroy', $solicitacao->id)}}" method="post">
             @csrf
             @method('DELETE')
-            <button class="btn btn-danger btn-sm">Delete</button>
-            
+            <button class="btn btn-danger btn-sm">Delete</button> 
             </form>
-        </li>  
-         
-     @endforeach
-    </ul>
+            </th>
+          </tr>
+          @endforeach
+        </tbody>
+    </table>
 
-
-     
-
-
-
-
+</div> 
 </x-layout>
-</div>
+
